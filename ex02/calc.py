3adfd3603c42b616febb0e48b05774af727a6542
Tsuzuki <c0a2108861@edu.teu.ac.jp>
2022-06-21 14:45:03 +0900

    3終了

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,10 +1,16 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 root = tk.Tk()            #ウィンドウの設定
 root.geometry("300x500")
-
+#-----------------------------------------
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
 #-----------------------------------------
 def generate_button(txt,rx,ry):              #ボタンのテキスト、x座標、y座標(0.0~1.0ウィンドウサイズ依存)
         button = tk.Button(root,text=txt,height=2,width=4,font=("Times New Roman",30))
+        button.bind("<1>",button_click)
         button.grid(row=rx,column=ry)
 #------------------------------------------ボタンを作る関数
 
