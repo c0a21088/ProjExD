8cb5f4b534741af2b4b74939996d3ae4503c0d8c
Tsuzuki <c0a2108861@edu.teu.ac.jp>
2022-06-21 14:40:55 +0900

    2終了

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,29 +4,14 @@ root.geometry("300x500")
 
 #-----------------------------------------
 def generate_button(txt,rx,ry):              #ボタンのテキスト、x座標、y座標(0.0~1.0ウィンドウサイズ依存)
-        button = tk.Button(root,text=txt,height=10,width=10)
-        button.pack()
-        button.place(relx=rx,rely=ry)
+        button = tk.Button(root,text=txt,height=2,width=4,font=("Times New Roman",30))
+        button.grid(row=rx,column=ry)
 #------------------------------------------ボタンを作る関数
 
 def generate_calc_button():
     for i in range(10):
-        if i <= 3:
-            ry=0
-        elif 3 < i <= 6:
-            ry=1/4
-        elif 6 < i <=9:
-            ry=2/4
-        elif i == 0:
-            ry=3/4
-        if i % 3 == 1:
-            rx=0
-        elif i % 3 == 2:
-            rx=1/3
-        elif i % 3 == 0:
-            rx=2/3
-        if i == 0:
-            ry=0
+        rx=i//3
+        ry=i%3
         generate_button(str(i),rx,ry)
 
 generate_calc_button()
