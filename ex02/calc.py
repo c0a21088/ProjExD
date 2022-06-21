23d7049140bfbba5cfa3d0edb523596e0484d8e6
Tsuzuki <c0a2108861@edu.teu.ac.jp>
2022-06-21 15:08:30 +0900

    4終了

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 root = tk.Tk()            #ウィンドウの設定
-root.geometry("300x500")
 #-----------------------------------------
 def button_click(event):
     btn = event.widget
@@ -11,14 +10,22 @@ def button_click(event):
 def generate_button(txt,rx,ry):              #ボタンのテキスト、x座標、y座標(0.0~1.0ウィンドウサイズ依存)
         button = tk.Button(root,text=txt,height=2,width=4,font=("Times New Roman",30))
         button.bind("<1>",button_click)
-        button.grid(row=rx,column=ry)
+        button.grid(row=rx+1,column=ry)
 #------------------------------------------ボタンを作る関数
-
+def generate_entery():
+    entry = tk.Entry(justify="right",width=10,font=("Times New Roman",40))
+    entry.insert(tk.END,"")
+    entry.grid(columnspan=3)
+    entry.grid(row=0)
+    
+#--------------------------------------------
 def generate_calc_button():
     for i in range(10):
-        rx=i//3
-        ry=i%3
+        k=10-i-1
+        rx=k//3
+        ry=k%3
         generate_button(str(i),rx,ry)
-
+#--------------------------------------------
 generate_calc_button()
+generate_entery()
 root.mainloop()
