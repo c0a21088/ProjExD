30e841357ea4b0685c524b199d592435e80df028
Tsuzuki <c0a2108861@edu.teu.ac.jp>
2022-06-28 14:26:41 +0900

    3回目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,9 +1,16 @@
 import tkinter as tk
 from tkinter import font
 import tkinter.messagebox as tkm
+
 if __name__ == "__main__":
-    root = tk.Tk("迷える工科トン")
+    root = tk.Tk()
+    root.title("迷える工科トン")
     canvas = tk.Canvas(root,width=1500,height=900,bg="black")
     canvas.pack()
+    
+    tori = tk.PhotoImage(file="fig/5.png")
+    cx, cy = 300,400
+    canvas.create_image(cx,cy, image=tori,tag=tori)
+
     root.mainloop()
     
