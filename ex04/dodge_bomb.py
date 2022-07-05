a4ccee84e9071425b3ae5f2da7af2ee6c5ef2040
Tsuzuki <c0a2108861@edu.teu.ac.jp>
2022-07-05 14:22:03 +0900

    こうかとんok

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,16 +7,21 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900)) #surface
     screen_rct = screen_sfc.get_rect()            #rect
+
     bgimg_sfc = pg.image.load("../ex03/fig/pg_bg.jpg") #surface
     bgimg_rct = bgimg_sfc.get_rect()              #rect
     screen_sfc.blit(bgimg_sfc,bgimg_rct)
-    
-    #pg.display.update()
-    #clock.tick(0.2) #フレームレート
 
+    kk_sfc = pg.image.load("../ex03/fig/6.png")
+    kk_sfc = pg.transform.rotozoom(kk_sfc, 0, 2.0)
+    kk_rct = kk_sfc.get_rect()
+    kk_rct.center = 900, 400
+    
+    
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
-        
+        screen_sfc.blit(kk_sfc, kk_rct)
+
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
